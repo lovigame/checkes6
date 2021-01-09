df5f42834064375f5af2b40f9e0b4f8e0687f95b
wb-wp721743 <wb-wp721743@alibaba-inc.com>
2021-01-09 11:48:41 +0800

    feat: 加了schema数据检测

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # checkes6
+
+## 安装
+    npm i -g @gswl/checkes6
+
+检查es6语法错误
+
+## 检查supermodel的schema里错误语法
+只有http或https会当schema数据，schema数据不检测${}语法
+
+    checkes6 http://dddd.json
+
+## 检查目录
+    checkes6 ./
--- src/commands/func.ts
+++ src/commands/func.ts
@@ -8,19 +8,114 @@ import prompts from "prompts";
 import rimraf from "rimraf";
 import through2 from "through2";
 import { getLogger } from "../logger";
+import fetch from 'node-fetch';
 
 const logger = getLogger("checkes6");
 
-const CheckArgs = ["let ","const ","=>","Proxy"];
+const CheckArgs = ["let ","const ","=>","Proxy","...","${","："];
 export default async (args: string[]) => {
-  const spanner = ora(`   正在拷贝...... `);
   const src = args[0];
-  fileDisplaySync(src);
-  
+  if(src.startsWith('http')){
+    fetch(src).then(async res=>{
+      let data = await res.text();
+      if(isSchema(data)){
+        checkSchema(data);
+        return;
+      }
+      let ret = getInValidData(data);
+      if(ret){
+        console.error('err  ','包含'+ret.msg);
+        return;
+      }
+      console.log("ok")
+    }).catch(e=>{
+      console.error(e);
+    })
+  }
+  else{
+    fileDisplaySync(src);
+  }
 };
 
+function stringHasInvalidArgs(str:string,args){
+  for(let i=0;i<args.length;++i){
+    let arg = args[i];
+    if(str.indexOf(arg) !== -1){
+      return true;
+    }
+  }
+  return false;
+}
+
+interface DynamicObj{
+  key:string;
+  expression:string;
+}
+
+interface SchemaObj{
+  children:SchemaObj[];
+  name:string;
+  dynamic:DynamicObj[];
+}
+
+function checkSchemaObj(obj:SchemaObj,parentKey:string){
+  let newArgs = CheckArgs.slice();
+  
+  let idx = newArgs.indexOf('${');
+  if(idx !== -1){
+    newArgs.splice(idx,1)
+  }
+  
+  obj.dynamic && obj.dynamic.forEach(d=>{
+    if(stringHasInvalidArgs(d.expression,newArgs)){
+      console.log(parentKey+"."+obj.name+"."+d.key,d.expression);
+    }
+  })
+  obj.children && obj.children.forEach(c=>{
+    checkSchemaObj(c,parentKey+"."+obj.name)
+  })
+  
+}
+
+function checkSchema(data){
+  let obj = JSON.parse(data);
+  let scenes = obj.scenes;
+  for(let i=0;i<scenes.length;++i){
+    let scene = scenes[i];
+    let layers = scene.layers;
+    for(let j=0;j<layers.length;++j){
+      let layer = layers[j];
+      checkSchemaObj(layer,scene.id+"."+scene.name);
+    }
+  }
+}
+
+function isSchema(data:string){
+  try{
+    let obj = JSON.parse(data);
+    if(obj.id && obj.scenes && obj.stageView){
+      return true;
+    }
+
+  }
+  catch(e){
+    
+  }
+  return false;
+}
 
 
+
+function getInValidData(data:any){
+  let obj = getSignPos(data);
+  let n = 0;
+    while(n<CheckArgs.length){
+      if(hasCheckArgs(data,CheckArgs[n],obj)){
+        return {msg:CheckArgs[n]};
+      }
+      n++;
+    }
+}
 
  function fileDisplaySync(filePath: string) {
    let hasErr:boolean = false;
@@ -40,18 +135,13 @@ export default async (args: string[]) => {
               let type = path.extname(filepath);
               if(type && type === '.js'){
                 let data = fs.readFileSync(filepath,'utf-8');
-                let obj = getSignPos(data);
-                let n = 0;
-                // fs.writeFileSync('C:/Users/admin/Desktop/tooltest/file2/test.js',data,'utf-8');
-                  while(n<CheckArgs.length){
-                    
-                    if(hasCheckArgs(data,CheckArgs[n],obj)){
-                      console.error('err  ','文件 '+filepath+' 包含'+CheckArgs[n]);
-                      hasErr = true;
-                      return;
-                    }
-                    n++;
-                  }
+                let ret = getInValidData(data);
+                if(ret){
+                  console.error('err  ','文件 '+filepath+' 包含'+ret.msg);
+                  hasErr = true;
+                  return;
+                }
+                
               }
             }
             if (isDir) {
